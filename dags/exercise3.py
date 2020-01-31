6cfe693baace03f1a9bd58bc60799ead3d63c77d
Mike Fennemore <mfennemore@schubergphilis.com>
2020-01-31 14:01:33 +0100

    Corrects exercise3 refactoring

--- dags/exercise3.py
+++ dags/exercise3.py
@@ -28,9 +28,6 @@ t5 = DummyOperator(task_id='the_end', dag=dag)
 wait_periods = ['1','5','10']
 for wait_period in wait_periods:
     t1 >> BashOperator(task_id='wait_'+wait_period, bash_command="sleep "+wait_period, dag=dag) >> t5
-#t2 = BashOperator(task_id='wait_5', bash_command="sleep 5", dag=dag)
-#t3 = BashOperator(task_id='wait_1', bash_command="sleep 1", dag=dag)
-#t4 = BashOperator(task_id='wait_10', bash_command="sleep 10", dag=dag)
-
-
-t1 >> [t2,t3,t4] >> t5
+# t2 = BashOperator(task_id='wait_5', bash_command="sleep 5", dag=dag)
+# t3 = BashOperator(task_id='wait_1', bash_command="sleep 1", dag=dag)
+# t4 = BashOperator(task_id='wait_10', bash_command="sleep 10", dag=dag)
