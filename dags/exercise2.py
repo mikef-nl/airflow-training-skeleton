cd66e4b483e39b850dc4583cc3693ac8d4a62ca6
Mike Fennemore <mfennemore@schubergphilis.com>
2020-01-31 12:00:24 +0100

    Adds exercise 2

---
+++ dags/exercise2.py
@@ -0,0 +1,23 @@
+import airflow
+import datetime
+from airflow.models import DAG
+from airflow.operators.dummy_operator import DummyOperator
+
+args = {
+    'owner': 'Mike',
+    'start_date': datetime.datetime(2020, 1, 311),
+}
+
+dag = DAG(
+    dag_id='exercise1',
+    default_args=args,
+    schedule_interval=@daily
+)
+
+t1 = DummyOperator(task_id='task1', dag=dag)
+t2 = DummyOperator(task_id='task2', dag=dag)
+t3 = DummyOperator(task_id='task3', dag=dag)
+t4 = DummyOperator(task_id='task4', dag=dag)
+t5 = DummyOperator(task_id='task5', dag=dag)
+
+t1 >> t2 >> [t3,t4] >> t5
