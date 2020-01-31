b12c2f21b20b97d3f70caf07b7509467d245ccd5
Mike Fennemore <mfennemore@schubergphilis.com>
2020-01-31 13:51:46 +0100

    Adds exercise3 refactoring

--- dags/exercise3.py
+++ dags/exercise3.py
@@ -23,9 +23,14 @@ t1 = PythonOperator(
     python_callable=_print_exec_date,
     provide_context=True,
     dag=dag)
-t2 = BashOperator(task_id='wait_5', bash_command="sleep 5", dag=dag)
-t3 = BashOperator(task_id='wait_1', bash_command="sleep 1", dag=dag)
-t4 = BashOperator(task_id='wait_10', bash_command="sleep 10", dag=dag)
+
 t5 = DummyOperator(task_id='the_end', dag=dag)
+wait_periods = [1,5,10]
+for wait_period in wait_periods:
+    t1 >> BashOperator(task_id='wait_'+wait_period, bash_command="sleep "+wait_period, dag=dag) >> t5
+#t2 = BashOperator(task_id='wait_5', bash_command="sleep 5", dag=dag)
+#t3 = BashOperator(task_id='wait_1', bash_command="sleep 1", dag=dag)
+#t4 = BashOperator(task_id='wait_10', bash_command="sleep 10", dag=dag)
+
 
 t1 >> [t2,t3,t4] >> t5
