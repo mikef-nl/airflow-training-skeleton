784fd052a7e54b2c4b9cfd708f4a76011ac0b33d
Mike Fennemore <mfennemore@schubergphilis.com>
2020-02-14 11:52:58 +0100

    Test

--- dags/operators/launch_library_operator.py
+++ dags/operators/launch_library_operator.py
@@ -4,13 +4,11 @@ from airflow.utils.decorators import apply_defaults
 
 class LaunchLibraryOperator(BaseOperator):
 
-    template_fields = ('_start_date','_end_date','_params','_result_path')
+    template_fields = ('_params','_result_path')
     
     @apply_defaults
-    def __init__(self, start_date, end_date, endpoint, conn_id, params, result_path, *args, **kwargs):        
+    def __init__(self, endpoint, conn_id, params, result_path, *args, **kwargs):        
         super().__init__(*args, **kwargs)
-        self._start_date = start_date
-        self._end_date = end_date
         self._endpoint = endpoint
         self.conn_id = conn_id
         self.params = params
